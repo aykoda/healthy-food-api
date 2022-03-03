1ba0ffe78cc4f1ed361518e18d043adc277485f3
Aykoda <aykodas@gmail.com>
2022-03-03 11:25:22 +0200

    v.1.1 yellow refactoring of Testing the all possible Controller's endpoints

--- src/test/java/com/techreturners/healthyfood/controller/MealManagerControllerTests.java
+++ src/test/java/com/techreturners/healthyfood/controller/MealManagerControllerTests.java
@@ -7,18 +7,24 @@ import com.techreturners.healthyfood.model.MealPlan;
 import com.techreturners.healthyfood.service.HealthyFoodServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -44,16 +50,25 @@ import static org.mockito.Mockito.when;
         mapper = new ObjectMapper();
     }
 
-    @Test
-     void getAResponse() throws Exception {
+   private static Stream<Arguments> parametersGenerator() {
+      return Stream.of(
+              Arguments.of("/api/v1/mealPlanner/",MockMvcResultMatchers.status().isOk()),
+              Arguments.of("/api/v1/mealPlanner/2000",MockMvcResultMatchers.status().isOk()),
+              Arguments.of("/api/v1/mealPlanner/badReq",MockMvcResultMatchers.status().isOk())
+      );
+   }
+
+   @ParameterizedTest
+   @MethodSource("parametersGenerator")
+     void getAResponse(String url, ResultMatcher result) throws Exception {
         MealPlan mealPlan =null;
         List<Meal> meals = new ArrayList<>();
 
         when(mockHealthyFoodServiceImpl.getDailyMeals(mealPlan)).thenReturn(meals);
 
         this.mockMvcController.perform(
-                        MockMvcRequestBuilders.get("/api/v1/mealPlanner/"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                        MockMvcRequestBuilders.get(url))
+                .andExpect(result);
     }
 
     }
